6ea224b8c24fefdf7dcd6c3b6662bd65afe7d237
Anders Solvang <anders@rubynor.com>
2021-05-30 12:46:23 +0200

    Add tailwind and fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("stylesheets/application.scss")
+import "@fortawesome/fontawesome-free/js/all";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
