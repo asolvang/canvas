4fc01794a81893eddb006d1368e15d89a7726bf1
Anders Solvang <anders@rubynor.com>
2021-05-30 13:13:34 +0200

    Add hotwire

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -12,5 +12,6 @@ require("stylesheets/application.scss")
 import "@fortawesome/fontawesome-free/js/all";
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
